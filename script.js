2ca17b60ec08179ca44eab0f950d81d08548f4f3
micael <micael.staeubli@gmail.com>
2023-01-25 11:53:38 +0100

    feat: delete button

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let myLeads = []
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const txtInput = document.getElementById("input")
 const ul_EL = document.getElementById("ulEl")
 
-let leadsLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadsLocalStorage){
   myLeads = leadsLocalStorage
   readerLeads()
 }
+
+deleteBtn.addEventListener("dblclick", function(){
+  localStorage.clear()
+  myLeads = []
+  readerLeads()
+})
 
 inputBtn.addEventListener("click", function(){
  myLeads.push(txtInput.value)
